18ad64172cea53c3bfe58d9c825ab2d164f995cc
xuanyu <969718197@qq.com>
2020-08-22 11:06:39 +0800

    打包去除所以console.log

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,20 @@
+const IS_PROD = ['production', 'prod'].includes(process.env.VUE_APP_ENV)
+
+// 去除 console.log
+let transformRemoveConsolePlugin = []
+if (IS_PROD) {
+  transformRemoveConsolePlugin = ['transform-remove-console']
+}
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@vue/cli-plugin-babel/preset', { useBuiltIns: 'usage', corejs: 3 }]
   ],
   plugins: [
     ['import', {
       libraryName: 'vant',
       libraryDirectory: 'es',
       style: true
-    }, 'vant']
+    }, 'vant'],
+    ...transformRemoveConsolePlugin
   ]
 }
