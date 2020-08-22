1aedcf5d6e26e0ddfb582ab0fff7d100da7677d9
xuanyu <969718197@qq.com>
2020-08-22 10:54:55 +0800

    添加了项目打包分析插件

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,14 @@
 const path = require('path')
-const { config } = require('process')
 const resolve = dir => path.join(__dirname, dir)
+const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin  // 打包分析
 
 module.exports = {
     publicPath: './', // 署应用包时的基本 URL。 vue-router hash 模式使用
     outputDir: 'dist', //  生产环境构建文件的目录
     assetsDir: 'static', //  outputDir的静态资源(js、css、img、fonts)目录
+    lintOnSave: !IS_PROD,
     productionSourceMap: false, // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
-    chainWebpack: config => {
-        // 别名路径
-        config.resolve.alias
-            .set('@', resolve('src'))
-            .set('assets', resolve('src/assets'))
-            .set('api', resolve('src/api'))
-            .set('layout', resolve('src/layouts'))
-            .set('views', resolve('src/views'))
-            .set('components', resolve('src/components'))
-    },
     css: {
         loaderOptions: {
             postcss: {
@@ -41,5 +33,23 @@ module.exports = {
                 ]
             }
         }
-    }
+    },
+    chainWebpack: config => {
+        // 别名路径
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('assets', resolve('src/assets'))
+            .set('api', resolve('src/api'))
+            .set('layout', resolve('src/layouts'))
+            .set('views', resolve('src/views'))
+            .set('components', resolve('src/components'))
+        // 打包分析
+        if (IS_PROD) {
+            config.plugin('webpack-report').use(BundleAnalyzerPlugin, [
+                {
+                    analyzerMode: 'static'
+                }
+            ])
+        }
+    },
 }
